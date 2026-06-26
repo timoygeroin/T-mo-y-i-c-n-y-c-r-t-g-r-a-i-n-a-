8d217c2fae2d9d04553fc9cc6188fcbc2be15c3b
Ťьi møý łìçħñíý şøřț ğęřøiñą <ti_moy_geroin@icloud.com>
2026-06-26 05:07:24 +0300

    Run post-merge sink router proof

--- platform/packages/manifestation-engine/src/manifestation-engine-proof.ts
+++ platform/packages/manifestation-engine/src/manifestation-engine-proof.ts
@@ -1,3 +1,5 @@
 import { runManifestationEngineProof } from "./index.js";
+import { runPostMergeSinkRouterProof } from "./post-merge-sink-router.js";
 
 runManifestationEngineProof();
+runPostMergeSinkRouterProof();
